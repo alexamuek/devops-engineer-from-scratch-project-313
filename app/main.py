5c1255c05f3e5204cb2462bd943c78316c387162
Alex <alexamuek@gmail.com>
2026-09-03 23:06:01 +0800

    fix redirect link

--- app/main.py
+++ app/main.py
@@ -74,7 +74,7 @@ def links_post():
     if not validate_body(link):
         return {"detail": {"message": "Invalid JSON body"}}, 422
     # сохраняем новую ссылку 
-    short_url = f"{os.getenv("BASE_URL")}r/{link["short_name"]}"
+    short_url = f"{os.getenv("BASE_URL")}/r/{link["short_name"]}"
     result = repo.add_link(link["original_url"], 
         link["short_name"], short_url)
     if result:
@@ -107,7 +107,7 @@ def links_patch(id):
     if not validate_body(link):
         return {"detail": {"message": "Invalid JSON body"}}, 422
     # сохраняем новую ссылку 
-    short_url = f"{os.getenv("BASE_URL")}r/{link["short_name"]}"
+    short_url = f"{os.getenv("BASE_URL")}/r/{link["short_name"]}"
     new_link = repo.update_link(id, link["original_url"], 
         link["short_name"], short_url)
     if new_link is None:
